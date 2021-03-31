4c9fd74644587aec2dbaf23097323356eb7f69ec
josealejolibreros <jose.libreros@correounivalle.edu.co>
2021-03-31 02:40:25 -0500

    Change 1
    Sleep time after executing searchButton script to avoid looking for results in an unloaded page

--- first.py
+++ first.py
@@ -2,18 +2,19 @@ from selenium import webdriver
 import time
 
 
-driver = webdriver.Chrome()
+driver = webdriver.Firefox()
 driver.get("https://google.co.in")
 time.sleep(1)
 
 input_element = driver.find_element_by_name("q")
 input_element.send_keys("test automation")
-time.sleep(1)
+
 script_text = "var searchButton = document.getElementsByName(\"btnK\")[1]; searchButton.click();"
 try:
     driver.execute_script(script_text)  # if it work, an alert with 'yes' should display
 except:
     pass
+time.sleep(2)
 
 results_div = driver.find_element_by_id('rso')
 results = results_div.find_elements_by_class_name("g")
